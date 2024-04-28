09a41c6b37bf227944bba6314c97ffb075992ea5
gytjd_omk5aqq <gytjd9516@naver.com>
2024-04-29 00:50:39 +0900

    feat: api 명세 수정

--- src/main/java/com/example/moim/user/controller/UserControllerDocs.java
+++ src/main/java/com/example/moim/user/controller/UserControllerDocs.java
@@ -1,21 +1,30 @@
 package com.example.moim.user.controller;
 
-import com.example.moim.user.dto.JoinInput;
-import com.example.moim.user.dto.MyClubOutput;
-import com.example.moim.user.dto.UserOutput;
-import com.example.moim.user.dto.userDetailsImpl;
+import com.example.moim.exception.ErrorResult;
+import com.example.moim.user.dto.*;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@Tag(name = "회원 api", description = "로그인 api는 /login, 입력값은 {\"email\":\"string\", \"password\":\"string\"} 로그인 성공시 응답 헤더 Authorization 에 jwt 발급")
+@Tag(name = "회원 api")
 public interface UserControllerDocs {
 
-    @Operation(summary = "회원가입", description = "필요 파라미터 : 이메일, 비밀번호")
-    String join(@RequestBody JoinInput joinInput);
+    @Operation(summary = "회원가입")
+    @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = String.class)))
+    @ApiResponse(responseCode = "409", description = "중복회원가입시 409 에러코드와 메세지 응답", content = @Content(schema = @Schema(implementation = ErrorResult.class)))
+    String signup(@RequestBody JoinInput joinInput);
+
+    @Operation(summary = "로그인", description = "로그인 성공시 응답 헤더 Authorization 에 jwt 발급")
+    @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema))
+    @ApiResponse(responseCode = "404", description = "로그인 정보 틀리면 404 에러코드와 메세지 응답", content = @Content(schema = @Schema(implementation = ErrorResult.class)))
+    void login(@RequestBody LoginInput loginInput, HttpServletResponse response);
 
     @Operation(summary = "마이페이지 회원 정보 조회")
     UserOutput userFind(@AuthenticationPrincipal userDetailsImpl userDetailsImpl);
