6599ba2ee5324c783b1932ce3af7a2c065bb7ed6
gytjd_omk5aqq <gytjd9516@naver.com>
2024-04-29 12:03:09 +0900

    feat: 유저 이름 본명만 사용

--- src/main/java/com/example/moim/user/dto/UserOutput.java
+++ src/main/java/com/example/moim/user/dto/UserOutput.java
@@ -17,7 +17,6 @@ public class UserOutput {
     public UserOutput(User user) {
         this.email = user.getEmail();
         this.name = user.getName();
-        this.nickname = user.getNickname();
         this.birthday = user.getBirthday();
         this.gender = user.getGender().toString();
         this.phone = user.getPhone();
