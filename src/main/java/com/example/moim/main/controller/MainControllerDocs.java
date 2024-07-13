08264f484f040d31ffbb020c730eb0487c366c97
gytjd_omk5aqq <gytjd9516@naver.com>
2024-07-13 23:37:52 +0900

    feat: 모임 가입 후 메인페이지

--- src/main/java/com/example/moim/main/controller/MainControllerDocs.java
+++ src/main/java/com/example/moim/main/controller/MainControllerDocs.java
@@ -1,14 +1,19 @@
 package com.example.moim.main.controller;
 
+import com.example.moim.main.dto.MainOutput;
 import com.example.moim.main.dto.NoClubMainOutput;
 import com.example.moim.user.dto.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Tag(name = "모임 가입 전 메인 페이지 api")
 public interface MainControllerDocs {
 
     @Operation(summary = "모임 가입 전 메인 페이지")
     NoClubMainOutput noClubMainPage(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl);
+
+    @Operation(summary = "모임 가입 후 메인 페이지")
+    MainOutput mainPage (@AuthenticationPrincipal UserDetailsImpl userDetailsImpl, @PathVariable Long clubId);
 }
---
+++ src/main/java/com/example/moim/main/dto/MainOutput.java
@@ -0,0 +1,33 @@
+package com.example.moim.main.dto;
+
+import com.example.moim.club.dto.ScheduleOutput;
+import com.example.moim.club.entity.Club;
+import lombok.Data;
+import org.springframework.core.io.FileUrlResource;
+
+import java.io.IOException;
+import java.util.Base64;
+import java.util.List;
+
+@Data
+public class MainOutput {
+    private Long clubId;
+    private String title;
+    private String explanation;
+    private String profileImg;
+    private List<ScheduleOutput> scheduleList;
+
+    public MainOutput(Club club, List<ScheduleOutput> scheduleList) {
+        this.clubId = club.getId();
+        this.title = club.getTitle();
+        this.explanation = club.getExplanation();
+        try {
+            if (club.getProfileImgPath() != null) {
+                this.profileImg = Base64.getEncoder().encodeToString(new FileUrlResource(club.getProfileImgPath()).getContentAsByteArray());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        this.scheduleList = scheduleList;
+    }
+}
