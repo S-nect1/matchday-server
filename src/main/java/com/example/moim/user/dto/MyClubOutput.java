b34dda911ac131c0467c6027a3d771cf123e3e4c
gytjd_omk5aqq <gytjd9516@naver.com>
2024-07-14 00:08:39 +0900

    docs: 유저가 속한 모임 조회 api 수정

--- src/main/java/com/example/moim/user/dto/MyClubOutput.java
+++ src/main/java/com/example/moim/user/dto/MyClubOutput.java
@@ -1,16 +1,31 @@
 package com.example.moim.user.dto;
 
-import com.example.moim.club.entity.UserClub;
+import com.example.moim.club.entity.Club;
 import lombok.Data;
+import org.springframework.core.io.FileUrlResource;
+
+import java.io.IOException;
+import java.util.Base64;
 
 @Data
 public class MyClubOutput {
     private Long clubId;
     private String clubName;
     private String explanation;
+    private String profileImg;
+    private int memberCount;
 
-    public MyClubOutput(UserClub userClub) {
-        this.clubId = userClub.getClub().getId();
-        this.clubName = userClub.getClub().getTitle();
+    public MyClubOutput(Club club) {
+        this.clubId = club.getId();
+        this.clubName = club.getTitle();
+        this.explanation = club.getExplanation();
+        try {
+            if (club.getProfileImgPath() != null) {
+                this.profileImg = Base64.getEncoder().encodeToString(new FileUrlResource(club.getProfileImgPath()).getContentAsByteArray());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        this.memberCount = club.getMemberCount();
     }
 }
