cd8035e12358e17b3880566df4fc343ccac82593
5nam <ohnam90@gmail.com>
2025-03-11 17:56:34 +0900

    test(#6): ClubRepository findBySearchCond 테스트 코드 추가

--- src/main/java/com/example/moim/club/dto/ClubInput.java
+++ src/main/java/com/example/moim/club/dto/ClubInput.java
@@ -1,11 +1,14 @@
 package com.example.moim.club.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
+@Builder
 public class ClubInput {
+
     @NotBlank(message = "모임 이름을 적어야합니다!")
     private String title;
     @NotBlank(message = "모임 설명을 적어야합니다!")
@@ -22,4 +25,5 @@ public class ClubInput {
     private MultipartFile profileImg;
     private String mainUniformColor;
     private String subUniformColor;
+
 }
--- src/main/java/com/example/moim/club/dto/ClubSearchCond.java
+++ src/main/java/com/example/moim/club/dto/ClubSearchCond.java
@@ -1,8 +1,10 @@
 package com.example.moim.club.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ClubSearchCond {
     private String category;
     private String search;
