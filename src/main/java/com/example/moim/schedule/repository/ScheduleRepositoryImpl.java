f5f364c14b3d59d54bed27eddfd40c314ad7e4ae
5nam <ohnam90@gmail.com>
2025-03-12 20:20:37 +0900

    chore(#9): h2, jwt, oauth properties git ignore 에 추가, h2, lombok test dependency 로 추가

--- src/main/java/com/example/moim/schedule/repository/ScheduleRepositoryImpl.java
+++ src/main/java/com/example/moim/schedule/repository/ScheduleRepositoryImpl.java
@@ -8,10 +8,11 @@ import jakarta.persistence.EntityManager;
 
 import java.time.LocalDateTime;
 import java.util.List;
+
+import static com.example.moim.schedule.entity.QSchedule.schedule;
 import static org.springframework.util.StringUtils.hasText;
 
 import static com.example.moim.club.entity.QClub.*;
-import static com.example.moim.club.entity.QSchedule.*;
 
 public class ScheduleRepositoryImpl implements ScheduleRepositoryCustom {
 
