f019daefbf920c2002bc8894ff8871be62d9c407
hojooo <ghwn5833@gmail.com>
2025-03-27 23:45:07 +0900

    refactor: 기존 controller BaseResponse로 감싸기

--- src/main/java/com/example/moim/match/controller/MatchController.java
+++ src/main/java/com/example/moim/match/controller/MatchController.java
@@ -1,6 +1,7 @@
 package com.example.moim.match.controller;
 
 import com.example.moim.club.repository.ClubRepository;
+import com.example.moim.global.exception.BaseResponse;
 import com.example.moim.global.exception.ResponseCode;
 import com.example.moim.match.dto.*;
 import com.example.moim.match.exception.advice.MatchControllerAdvice;
@@ -25,93 +26,99 @@ public class MatchController {
     //매치 등록(생성) 신청(참가)
     //매치 생성
     @PostMapping("/match")
-    public MatchOutput matchSave(@RequestBody @Valid MatchInput matchInput, @AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
-        return matchService.saveMatch(userDetailsImpl.getUser(), matchInput);
+    public BaseResponse<MatchOutput> matchSave(@RequestBody @Valid MatchInput matchInput, @AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
+        return BaseResponse.onSuccess(matchService.saveMatch(userDetailsImpl.getUser(), matchInput), ResponseCode.OK);
     }
 
     //매치 등록
     @PatchMapping(value = "/match", produces = MediaType.APPLICATION_JSON_VALUE)
-    public MatchRegOutput matchRegister(@RequestBody @Valid MatchRegInput matchRegInput,
+    public BaseResponse<MatchRegOutput> matchRegister(@RequestBody @Valid MatchRegInput matchRegInput,
                                         @AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
-        return matchService.registerMatch(userDetailsImpl.getUser(), matchRegInput);
+        return BaseResponse.onSuccess(matchService.registerMatch(userDetailsImpl.getUser(), matchRegInput), ResponseCode.OK);
     }
 
     //매치 신청 생성
     @PostMapping("/match-apply")
-    public MatchApplyOutput matchApplySave(@RequestParam Long matchId,
+    public BaseResponse<MatchApplyOutput> matchApplySave(@RequestParam Long matchId,
                                            @RequestParam Long clubId,
                                            @AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
-        return matchService.saveMatchApp(userDetailsImpl.getUser(), matchId, clubId);
+        return BaseResponse.onSuccess(matchService.saveMatchApp(userDetailsImpl.getUser(), matchId, clubId), ResponseCode.OK);
     }
 
     //매치 신청 완료
     @PatchMapping("/match-apply")
-    public MatchApplyOutput matchApply(@RequestBody @Valid MatchApplyInput matchApplyInput,
+    public BaseResponse<MatchApplyOutput> matchApply(@RequestBody @Valid MatchApplyInput matchApplyInput,
                                        @AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
-        return matchService.applyMatch(userDetailsImpl.getUser(), matchApplyInput);
+        return BaseResponse.onSuccess(matchService.applyMatch(userDetailsImpl.getUser(), matchApplyInput), ResponseCode.OK);
     }
 
     //매치 초청
     @PostMapping("/match-invite")
-    public ResponseEntity<String> matchInvite(@RequestParam Long matchId,
+    public BaseResponse<String> matchInvite(@RequestParam Long matchId,
                                               @RequestParam Long clubId,
                                               @AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
         matchService.inviteMatch(userDetailsImpl.getUser(), matchId, clubId);
-        return ResponseEntity.ok("매치 초청 알림이 전송되었습니다.");
+        return BaseResponse.onSuccess("매치 초청 알림이 전송되었습니다.", ResponseCode.OK);
     }
 
     //매치 확정
     @PostMapping("/match-confirm")
-    public MatchConfirmOutput matchConfirm(@RequestParam Long matchId,
+    public BaseResponse<MatchConfirmOutput> matchConfirm(@RequestParam Long matchId,
                                            @RequestParam Long clubId,
                                            @AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
-        return matchService.confirmMatch(matchId, clubId, userDetailsImpl.getUser());
+        return BaseResponse.onSuccess(matchService.confirmMatch(matchId, clubId, userDetailsImpl.getUser()), ResponseCode.OK);
     }
 
     //등록된 매치 검색
     @GetMapping("/matches")
-    public List<MatchSearchOutput> findMatches(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
+    public BaseResponse<List<MatchSearchOutput>> findMatches(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
                                                @ModelAttribute MatchSearchCond matchSearchCond) {
-        return matchService.searchMatch(matchSearchCond);
+        return BaseResponse.onSuccess(matchService.searchMatch(matchSearchCond), ResponseCode.OK);
     }
 
     //확정된 매치리스트
     @GetMapping("/{clubId}/matches")
-    public List<ConfirmedMatchOutput> findConfirmedMatches(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
+    public BaseResponse<List<ConfirmedMatchOutput>> findConfirmedMatches(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
                                                            @PathVariable Long clubId) {
-        return matchService.findConfirmedMatch(clubRepository.findById(clubId).orElseThrow(() -> new MatchControllerAdvice(ResponseCode.CLUB_NOT_FOUND)));
+        return BaseResponse.onSuccess(matchService.findConfirmedMatch(clubRepository.findById(clubId)
+                .orElseThrow(() -> new MatchControllerAdvice(ResponseCode.CLUB_NOT_FOUND))),
+                ResponseCode.OK);
     }
 
     //활동 지역 소재 모임 리스트
     @GetMapping("/{clubId}/match-clubs")
-    public List<MatchClubOutput> findMatchClubs(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
+    public BaseResponse<List<MatchClubOutput>> findMatchClubs(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
                                                 @ModelAttribute MatchClubSearchCond matchClubSearchCond,
                                                 @PathVariable Long clubId) {
-        return matchService.searchMatchClubs(matchClubSearchCond, clubRepository.findById(clubId).orElseThrow(() -> new MatchControllerAdvice(ResponseCode.CLUB_NOT_FOUND)));
+        return BaseResponse.onSuccess(matchService.searchMatchClubs(matchClubSearchCond, clubRepository.findById(clubId)
+                .orElseThrow(() -> new MatchControllerAdvice(ResponseCode.CLUB_NOT_FOUND))),
+                ResponseCode.OK);
     }
 
     //매치 등록/신청 현황
     @GetMapping("/{clubId}/match-status")
-    public List<MatchStatusOutput> getMatchStatus(@PathVariable Long clubId) {
-        return matchService.findMatchStatus(clubRepository.findById(clubId)
-                .orElseThrow(() -> new MatchControllerAdvice(ResponseCode.CLUB_NOT_FOUND)));
+    public BaseResponse<List<MatchStatusOutput>> getMatchStatus(@PathVariable Long clubId) {
+        return BaseResponse.onSuccess(matchService.findMatchStatus(clubRepository.findById(clubId)
+                .orElseThrow(() -> new MatchControllerAdvice(ResponseCode.CLUB_NOT_FOUND))),
+                ResponseCode.OK);
     }
 
     //매치 결과 기록
     @PatchMapping(value = "/match/{matchId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public MatchRecordOutput matchRecordSave(@PathVariable Long matchId,
+    public BaseResponse<MatchRecordOutput> matchRecordSave(@PathVariable Long matchId,
                                              @RequestBody @Valid MatchRecordInput matchRecordInput,
                                              @AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
-        return matchService.saveMatchRecord(matchRepository.findById(matchId)
+        return BaseResponse.onSuccess(matchService.saveMatchRecord(matchRepository.findById(matchId)
                         .orElseThrow(() -> new MatchControllerAdvice(ResponseCode.MATCH_NOT_FOUND)),
                 userDetailsImpl.getUser(),
-                matchRecordInput);
+                matchRecordInput),
+                ResponseCode.OK);
     }
 
     //매치 메인 페이지(대시보드)
     @GetMapping("match/main/{clubId}")
-    public MatchMainOutput findMatchMain(@PathVariable Long clubId, @AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
-        return matchService.matchMainFind(clubId, userDetailsImpl.getUser());
+    public BaseResponse<MatchMainOutput> findMatchMain(@PathVariable Long clubId, @AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
+        return BaseResponse.onSuccess(matchService.matchMainFind(clubId, userDetailsImpl.getUser()), ResponseCode.OK);
     }
 
 }
