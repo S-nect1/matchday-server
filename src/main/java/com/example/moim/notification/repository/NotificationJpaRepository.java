f9913ea84f90c13400af71a02cbc62333779e74c
yebin <dev.devin.kr@gmail.com>
2025-04-10 06:48:51 +0900

    refactor(#20): 알림 레포지토리 의존성 역전

--- src/main/java/com/example/moim/notification/repository/NotificationJpaRepository.java
+++ src/main/java/com/example/moim/notification/repository/NotificationJpaRepository.java
@@ -2,14 +2,11 @@ package com.example.moim.notification.repository;
 
 import com.example.moim.notification.entity.NotificationEntity;
 import com.example.moim.user.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
-@Repository
-public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
+public interface NotificationJpaRepository extends JpaRepository<NotificationEntity, Long> {
     @Transactional(readOnly = true)
     Boolean existsByTargetUserAndIsRead(User user, Boolean isRead);
 
