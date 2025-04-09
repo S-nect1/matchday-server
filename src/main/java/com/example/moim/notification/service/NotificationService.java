8eaeeea9f840538d3a4d148224933bfeb427795e
yebin <dev.devin.kr@gmail.com>
2025-04-10 06:23:40 +0900

    refactor(#20): 알림 저장 애플리케이션 로직 구현

--- src/main/java/com/example/moim/notification/service/NotificationService.java
+++ src/main/java/com/example/moim/notification/service/NotificationService.java
@@ -2,13 +2,14 @@ package com.example.moim.notification.service;
 
 import com.example.moim.notification.dto.NotificationExistOutput;
 import com.example.moim.notification.dto.NotificationOutput;
+import com.example.moim.notification.entity.NotificationEntity;
 import com.example.moim.notification.repository.NotificationRepository;
+import com.example.moim.notification.repository.NotificationSender;
 import com.example.moim.user.entity.User;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,19 +17,22 @@ public class NotificationService {
     private final NotificationRepository notificationRepository;
     private final NotificationSender notificationSender;
 
-    public NotificationExistOutput checkNotice(User user) {
     public NotificationExistOutput checkUnread(User user) {
         return new NotificationExistOutput(notificationRepository.existsByTargetUserAndIsRead(user, false));
     }
 
-    public List<NotificationOutput> findNotice(User user) {
     public List<NotificationOutput> findAll(User user) {
         return notificationRepository.findByTargetUser(user).stream().map(NotificationOutput::new).toList();
     }
 
     @Transactional
-    public void removeNotice(Long id) {
     public void remove(Long id) {
         notificationRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void sendAll(List<NotificationEntity> notificationEntities) {
+        notificationRepository.saveAll(notificationEntities);
+        notificationEntities.forEach(notificationSender::send);
     }
 }
