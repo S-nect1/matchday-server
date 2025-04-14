095b3adff9482239f2d652a21edd949291ba0e5f
5nam <ohnam90@gmail.com>
2025-04-14 15:53:49 +0900

    refactor(#33): ScheduleInput Valid 추가

--- src/main/java/com/example/moim/schedule/dto/ScheduleInput.java
+++ src/main/java/com/example/moim/schedule/dto/ScheduleInput.java
@@ -13,6 +13,7 @@ import java.time.LocalDateTime;
 
 @Data
 public class ScheduleInput {
+    @NotBlank(message = "클럽 아이디를 입력해주세요.")
     private Long clubId;
     @NotBlank(message = "일정 제목을 입력해주세요.")
     private String title;
@@ -20,20 +21,20 @@ public class ScheduleInput {
     private String location;
     @Schema(pattern = "yyyy-MM-dd HH:mm", description = "yyyy-MM-dd HH:mm")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
-    @NotNull(message = "일정 시작 시간을 입력해주세요.")
+    @NotBlank(message = "일정 시작 시간을 입력해주세요.")
     private LocalDateTime startTime;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
-    @NotNull(message = "일정 종료 시간을 입력해주세요.")
+    @NotBlank(message = "일정 종료 시간을 입력해주세요.")
     private LocalDateTime endTime;
     @Min(value = 1, message = "참여 인원은 1명 이상이어야 합니다")
-    @NotNull(message = "최소 참여 인원을 입력해주세요.")
+    @NotBlank(message = "최소 참여 인원을 입력해주세요.")
     private int minPeople;//참여인원수
     @NotBlank(message = "일정 카테고리를 입력해 주세요.")
     private String category;
     private String note;
 
     @Builder
-    public ScheduleInput(Long clubId, String title, String location, @NotNull(message = "일정 시작 시간을 입력해주세요.") LocalDateTime startTime, @NotNull(message = "일정 종료 시간을 입력해주세요.") LocalDateTime endTime, @NotNull(message = "최소 참여 인원을 입력해주세요.") int minPeople, String category, String note) {
+    public ScheduleInput(Long clubId, String title, String location, LocalDateTime startTime, LocalDateTime endTime, int minPeople, String category, String note) {
         this.clubId = clubId;
         this.title = title;
         this.location = location;
