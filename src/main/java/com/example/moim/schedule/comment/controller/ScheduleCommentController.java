38f1291b0ed200ff19dc3786042e4c33d4e26381
5nam <ohnam90@gmail.com>
2025-05-15 23:59:30 +0900

    chore(#33): 주석 제거

--- src/main/java/com/example/moim/schedule/comment/controller/ScheduleCommentController.java
+++ src/main/java/com/example/moim/schedule/comment/controller/ScheduleCommentController.java
@@ -6,8 +6,6 @@ import com.example.moim.schedule.comment.dto.CommentOutput;
 import com.example.moim.schedule.comment.service.ScheduleCommentCommandService;
 import com.example.moim.schedule.comment.dto.CommentInput;
 import com.example.moim.user.dto.UserDetailsImpl;
-import com.example.moim.user.entity.User;
-import com.example.moim.user.repository.UserRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -22,18 +20,8 @@ public class ScheduleCommentController {
 
     private final ScheduleCommentCommandService scheduleCommentCommandService;
 
-    /**
-     * FIXME: 컨트롤러 수동테스트를 위해 필요한 코드, 추후에 지울 것.
-     */
-    private final UserRepository userRepository;
-
     @PostMapping("/schedules/{id}/comments")
     public BaseResponse<CommentOutput> scheduleComment(@RequestBody @Valid CommentInput commentInput, @PathVariable("id") Long scheduleId, @AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
-        /**
-         * FIXME: 컨트롤러 수동테스트를 위해 필요한 코드, 추후에 지울 것.
-         */
-//    public BaseResponse<CommentOutput> scheduleComment(@RequestBody @Valid CommentInput commentInput, @PathVariable("id") Long scheduleId) {
-//        User user = userRepository.findById(3L).get();
         CommentOutput commentOutput = scheduleCommentCommandService.saveComment(commentInput, scheduleId, userDetailsImpl.getUser());
         return BaseResponse.onSuccess(commentOutput, ResponseCode.OK);
     }
