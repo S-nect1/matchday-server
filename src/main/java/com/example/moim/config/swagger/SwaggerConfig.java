d33fbee02146005103bbf6af9a2a8f91889db7f4
hojooo <ghwn5833@gmail.com>
2025-08-03 13:12:02 +0900

    docs: add swagger groupedOpenApi

--- src/main/java/com/example/moim/config/swagger/SwaggerConfig.java
+++ src/main/java/com/example/moim/config/swagger/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -30,5 +31,43 @@ public class SwaggerConfig {
                 .components(new Components()
                         .addSecuritySchemes("BearerAuth", bearerAuthScheme))
                 .addSecurityItem(securityRequirement);
+    }
+
+    private GroupedOpenApi buildGroupedOpenApi(String group, String basePackage) {
+        return GroupedOpenApi.builder()
+                .group(group)
+                .pathsToMatch("/api/v1/**")
+                .packagesToScan(basePackage)
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi moinApi() {
+        return buildGroupedOpenApi("모임 관련 API @오남의", "com.example.moim.club.controller");
+    }
+
+    @Bean
+    public GroupedOpenApi notificationApi() {
+        return buildGroupedOpenApi("알림 관련 API @최예빈", "com.example.moim.notification.controller");
+    }
+
+    @Bean
+    public GroupedOpenApi matchApi() {
+        return buildGroupedOpenApi("매치 관련 API @문호주", "com.example.moim.match.controller");
+    }
+
+    @Bean
+    public GroupedOpenApi statisticApi() {
+        return buildGroupedOpenApi("전적 관련 API@문호주", "com.example.moim.statistic.controller");
+    }
+
+    @Bean
+    public GroupedOpenApi scheduleApi() {
+        return buildGroupedOpenApi("스케줄 관련 API@오남의", "com.example.moim.schedule.controller");
+    }
+
+    @Bean
+    public GroupedOpenApi userApi() {
+        return buildGroupedOpenApi("유저 관련 API@최예빈", "com.example.moim.user.controller");
     }
 }
